d99f90dbe197980a312dcbbcd500ed64962e4565
Rony Pablo <ronyalexis0@gmail.com>
2023-08-22 17:24:09 -0700

    Style

--- App.js
+++ App.js
@@ -1,11 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, SafeAreaView } from 'react-native';
 
 export default function App() {
   return (
-    <View style={{flex:1, alignItems: 'center', justifyContent: 'center'}}>
-      <Text>Our react native blank canvas</Text>
-    </View>
+    <SafeAreaView style={{flex:1, justifyContent: 'center', backgroundColor: 'tan'}}>
+      <Text style={{backgroundColor:'green', paddingVertical:4}}>Search</Text>
+      <Text style={{flex:.8, backgroundColor:'blue', paddingVertical:4}}>List</Text>
+    </SafeAreaView>
   );
 }
 
