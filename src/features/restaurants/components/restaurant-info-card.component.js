1660aea084236ce2d5a9ff4b60d43e46bd945363
Rony Pablo <ronyalexis@gmail.com>
2023-11-20 11:55:38 -0800

    adding more icons and adjusting spacing

--- src/features/restaurants/components/restaurant-info-card.component.js
+++ src/features/restaurants/components/restaurant-info-card.component.js
@@ -1,8 +1,10 @@
 import React from "react";
 import styled from "styled-components/native";
-import { Text, StyleSheet } from "react-native";
+import { Text, StyleSheet, Image } from "react-native";
 import { Card } from "react-native-paper";
 import { SvgXml } from "react-native-svg";
+
+import open from "../../../../assets/open.js";
 
 import star from "../../../../assets/star.js";
 
@@ -30,25 +32,53 @@ const Info = styled.View`
   padding: ${(props) => props.theme.space[3]};
 `;
 
+const RestaurantIcons = styled.View`
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+const Rating = styled.View`
+  flex-direction: row;
+  padding: ${(props) => props.theme.space[2]} 0;
+`;
+
 export const RestaurantInfoCard = ({ restaurant = {} }) => {
   const {
     name = "Some Restaurant",
-    icon,
+    icon = "https://maps.gstatic.com/mapfiles/place_api/icons/v1/png_71/lodging-71.png",
     photos = [
       "https://www.foodiesfeed.com/wp-content/uploads/2019/06/top-view-for-box-of-2-burgers-home-made-600x899.jpg",
     ],
     address = "100 some random street",
     isOpenNow = true,
     rating = 4,
-    isClosedTemporarily,
+    isClosedTemporarily = true,
   } = restaurant;
+
+  const ratingArray = Array.from(new Array(Math.floor(rating)));
+  // this is to make a new array with elements amounts of rating, elements of undefined, math.ceil to avoid decimals and just round to whole numbers UP, math.floor to round DOWN
 
   return (
     <RestaurantCard elevation={5}>
       <RestaurantCardCover key={name} source={{ uri: photos[0] }} />
       <Info>
         <Title>{name}</Title>
-        <SvgXml xml={star} width={20} height={20} />
+        <RestaurantIcons>
+          <Rating>
+            {ratingArray.map(() => (
+              <SvgXml xml={star} width={20} height={20} />
+            ))}
+          </Rating>
+          {isClosedTemporarily && (
+            <Text variant="label" style={{ color: "red" }}>
+              CLOSED TEMPORARILY
+            </Text>
+          )}
+          {isOpenNow && <SvgXml xml={open} width={20} height={20} />}
+          <Image style={{ width: 15, height: 15 }} source={{ uri: icon }} />
+        </RestaurantIcons>
+
         <Address>{address}</Address>
       </Info>
     </RestaurantCard>
