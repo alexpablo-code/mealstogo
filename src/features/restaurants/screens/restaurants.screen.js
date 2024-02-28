f3d3b77bab930705faaf443feaeaf47d6539aa4a
Rony Pablo <ronyalexis@gmail.com>
2024-02-28 14:03:31 -0800

    adding loading activity with conditional

--- src/features/restaurants/screens/restaurants.screen.js
+++ src/features/restaurants/screens/restaurants.screen.js
@@ -2,7 +2,7 @@ import React, { useContext } from "react";
 import { useState } from "react";
 import styled from "styled-components/native";
 import { FlatList } from "react-native";
-import { Searchbar } from "react-native-paper";
+import { Searchbar, ActivityIndicator, MD2Colors } from "react-native-paper";
 import { RestaurantInfoCard } from "../components/restaurant-info-card.component";
 
 import { SafeArea } from "../../../components/utility/safe-area.component";
@@ -19,6 +19,16 @@ const RestaurantListContainer = styled.View`
   background-color: ${(props) => props.theme.colors.brand.secondary};
 `;
 
+const Loading = styled(ActivityIndicator)`
+  margin-left: -25px;
+`;
+
+const LoadingContainer = styled.View`
+  position: absolute;
+  top: 50%;
+  left: 50%;
+`;
+
 export const RestaurantsScreen = () => {
   const { restaurants, isLoading, error } = useContext(RestaurantsContext);
   return (
@@ -26,6 +36,14 @@ export const RestaurantsScreen = () => {
       <SearchContainer>
         <Searchbar />
       </SearchContainer>
+      {isLoading ? (
+        <ActivityIndicator animating={true} color={MD2Colors.red800} />
+      ) : null}
+      {isLoading && (
+        <LoadingContainer>
+          <Loading size={50} animating={true} color={MD2Colors.red800} />
+        </LoadingContainer>
+      )}
       <FlatList
         data={restaurants}
         renderItem={({ item }) => <RestaurantInfoCard restaurant={item} />}
